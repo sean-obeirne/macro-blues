f4d96c1c1c44079609f970cc66c5e4a173f92545
Sean O'Beirne <sean.t.obeirne@gmail.com>
2026-04-26 12:09:26 -0400

    rs fix

--- macro-blues/input/keymap.c
+++ macro-blues/input/keymap.c
@@ -18,10 +18,10 @@ const uint8_t keymap[NUM_KEYS] = {
     [6] = HID_KEY_NONE,
     [9] = HID_KEY_NONE,
     /* middle row */
-    [1] = HID_KEY_F1,
-    [4] = HID_KEY_F2,
-    [7] = HID_KEY_F3,
-    [10] = HID_KEY_F4,
+    [1] = HID_KEY_F4,
+    [4] = HID_KEY_F3,
+    [7] = HID_KEY_F2,
+    [10] = HID_KEY_F1,
     /* bottom row */
     [2] = HID_KEY_F5,
     [5] = HID_KEY_F6,
